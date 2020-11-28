2b1fb477e6b350d9fe05a574aa0dd77d0591d7e9
catpsg <74920611+catpsg@users.noreply.github.com>
2020-11-28 17:38:32 +0530

    Add files via upload

---
+++ templates/actions.js
@@ -0,0 +1,47 @@
+$(function(){
+    $(".fold-table tr.view").on("click", function(){
+      $(this).toggleClass("open").next(".fold").toggleClass("open");
+    });
+  });
+
+  function myFunction() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[1];
+    
+      if (td ) {
+        txtValue = td.textContent || td.innerText;
+        
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+  }
+
+ function myyFunction() {
+   
+    var value = $(this).val;
+   
+    $("table tbody tr").each(function(index) {
+        if (index !== 0) {
+
+            $row = $(this);
+
+            var id = $row.find("td:first").text();
+
+            if (id.indexOf(value) !== 0) {
+                $row.hide();
+            }
+            else {
+                $row.show();
+            }
+        }
+    });
+}
